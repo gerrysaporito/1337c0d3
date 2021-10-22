14dbbea2a960e5f1a5f664f5d4cb2d6e6654dac7
Gerry Saporito <gerrysaporito@gmail.com>
2021-10-22 12:08:13 -0400

    Update Leet 0210 - Course Scheduler 2.ts

--- Leet 0210 - Course Scheduler 2.ts
+++ Leet 0210 - Course Scheduler 2.ts
@@ -1,42 +1,39 @@
-/*
- * Topological Sort
- */
 function findOrder(numCourses: number, prerequisites: number[][]): number[] {
     const generateAdjList = (prerequisites: number[][]) => {
         const adjList = {};
         for (const [want, req] of prerequisites) {
             if (want in adjList) adjList[want].push(req);
             else adjList[want] = [req];
-            if (!(req in adjList)) adjList[req] = [];
         }
         return adjList;
     }
     const isAcyclic = (course: number): boolean => {
-        if (cycle.has(course)) return false;
         if (visited.has(course)) return true;
+        if (!(course in adjList)) {
+            visited.add(course);
+            res.push(course);
+            return true
+        }
+        if (cycle.has(course)) return false;
         
         cycle.add(course);
         for (const req of adjList[course]) {
             if (!isAcyclic(req)) return false;
         }
         cycle.delete(course);
-        
         visited.add(course);
         res.push(course);
-        return true;
+        return true
     }
     
+    const adjList = generateAdjList(prerequisites);
     const visited = new Set();
     const cycle = new Set();
-    const adjList = generateAdjList(prerequisites);
     const res = [];
-
     for (let i = 0; i < numCourses; ++i) {
-        if (!(i in adjList)) res.push(i);
-        else if (!isAcyclic(i)) return [];
+        if (!isAcyclic(i)) return []
     }
-    
-    return res;
+    return res
 };
 
 /* 
